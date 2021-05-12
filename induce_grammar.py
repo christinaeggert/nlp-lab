22015c99a359ff4f1776d1943b8b8ba2c2510049
Christina Eggert <chreg605@student.liu.se>
2021-05-12 16:47:29 +0200

    use dict for rules, use stderr, correct amount of white spaces

--- induce_grammar.py
+++ induce_grammar.py
@@ -1,30 +1,47 @@
-from collections import Counter
+import re
 import sys
+
+
+# merges dict2 into dict1
+def merge_rules(dict1, dict2):
+    for root in dict2:
+        if root in dict1:
+            for word in dict2[root]:
+                if word in dict1[root]:
+                    dict1[root][word] = dict1[root][word] + dict2[root][word]
+                else:
+                    dict1[root][word] = dict2[root][word]
+        else:
+            dict1[root] = dict2[root]
+    return dict1
 
 
 def get_rules(branch):
     if branch == '':
-        return 'err', [], [], [], []
-    rl = []
-    rr = []
-    v = set()
-    n = []
+        return 'err', {}, {}, [], []
+    rr = {}  # rules
+    rl = {}  # lexical rules
+    v = set()  # words
+    # n = []  # notterminals
     # only two symbols left: A v
     leaves = branch.split()
     if len(leaves) == 2:
         # save lexical rule and word
-        rl.append(leaves[0] + ' ' + leaves[1])
+        rl[leaves[0]] = {
+            'root_count': 1,
+            leaves[1]: 1
+        }
         v.add(leaves[1])
-        n.append(leaves[0])
+        # n.append(leaves[0])  # even necessary anymore?
 
-        return leaves[0], rl, rr, v, n
+        return leaves[0], rl, rr, v  # , n
     elif len(leaves) < 2:
-        #print('ERROR: Something went wrong if there is only one symbol left. ', leaves)
-        return 'err', [], [], [], []
+        print('ERROR: Something went wrong if there is only one symbol left. ', leaves, file=sys.stderr)
+        return 'err', [], [], []  # , []
 
     # A -> A ...
     root, branches = branch.split(' ', 1)
-    n.append(root)
+    # n.append(root)
     # find the branches by matching parentheses
     opening = 0
     closing = 0
@@ -40,109 +57,153 @@ def get_rules(branch):
                 cut = i + 2
 
     if closing != opening:
-        #print('ERROR: The number of opening and closing parentheses does not add up.')
-        return 'err', [], [], [], []
+        print('ERROR: The number of opening and closing parentheses does not add up.', file=sys.stderr)
+        return 'err', [], [], []  # , []
 
     # get next rule
-    rule = root + ' ->'
+    right_side = []
     for i in range(len(br)):
         # call function for new found branch without the outmost parentheses
-        nonterminal, rulesl, rulesr, words, nonterminals = get_rules(br[i][1:len(br[i]) - 1])
+        nonterminal, rulesl, rulesr, words = get_rules(br[i][1:len(br[i]) - 1])
         # don't use illegal tree for the grammar
         if nonterminal == 'err':
-            return 'err', [], [], [], []
-        rule = rule + ' ' + nonterminal
+            return 'err', {}, {}, []  # , []
+        right_side.append(nonterminal)
         # save rules and words from lower branches
-        rl = rl + rulesl
-        rr = rr + rulesr
+        rl = merge_rules(rl, rulesl)
+        rr = merge_rules(rr, rulesr)
         v.update(words)
-        n = n + nonterminals
+        # n = n + nonterminals
+    right_side = tuple(right_side)
 
     # add new rule
-    rr.append(rule)
+    rule = {
+        root: {
+            'root_count': 1,
+            right_side: 1
+        }
+    }
+    rr = merge_rules(rr, rule)
 
-    return root, rl, rr, v, n
+    return root, rl, rr, v  # , n
 
 
 def induce_grammar(name):
     s = ''  # start symbol
-    rr = []  # rules
-    rl = []  # lexical rules
+    rr = {}  # rules
+    rl = {}  # lexical rules
     v = set()  # words
-    n = []  # notterminals
-    # import data from file
-    # f = open(file)
-    # line = 1
-    # lines = len(f.readlines())
-    # f.close()
+    # n = []  # notterminals
     # iterate over all training data
     for tree in sys.stdin:
-        root, rulesl, rulesr, words, notterminals = get_rules(tree[1:len(tree) - 2])
+        # ignore everything before the first opening parenthesis
+        i = 0
+        while tree[i] != '(':
+            i = i + 1
+        tree = tree[i:len(tree)]
+        # insert and or remove white spaces
+        problems = re.findall(r'\w\(', tree)
+        for combo in problems:
+            tree = re.sub(combo[0] + r'\(', combo[0] + ' (', tree)
+        tree = re.sub(r' +', ' ', tree)
+        tree = re.sub(r'\( ', '(', tree)
+        tree = re.sub(r' \)', ')', tree)
+        root, rulesl, rulesr, words = get_rules(tree[1:len(tree) - 2])
 
         if root == 'err':
             continue
 
         if s != '':
             if s != root:
-                #print('ERROR: start symbol changed from ', s, ' to ', root)
+                print('ERROR: start symbol changed from ', s, ' to ', root, file=sys.stderr)
                 # don't use this tree
                 continue
         else:
             s = root
 
         # add rules and symbols to grammar
-        rr = rr + rulesr
-        rl = rl + rulesl
+        rr = merge_rules(rr, rulesr)
+        rl = merge_rules(rl, rulesl)
         v.update(words)
-        n = n + notterminals
-        # print(line, '/', lines, ' trees', end="\r")
-        # line = line + 1
+        # n = n + notterminals
 
-    nrr = Counter(sorted(rr))
-    nrl = Counter(sorted(rl))
-    nn = Counter(n)
     w = sorted(v)
 
     # calculate probabilities and write to stdout
     if name == '':
-        for key in nrr:
-            root, rest = key.split(' ', 1)
-            p = nrr[key] / nn[root]
-            if p % 1 > 0:
-                print(key, p)
+        for root in rr.keys():
+            if root in rl:
+                total = rr[root]['root_count'] + rl[root]['root_count']
             else:
-                print(key, "{:d}".format(int(p)))
-            
-        for key in nrl:
-            root, rest = key.split(' ', 1)
-            p = nrl[key] / nn[root]
-            if p % 1 > 0:
-                print(key, p)
+                total = rr[root]['root_count']
+            for right in rr[root].keys():
+                if right == 'root_count':
+                    continue
+
+                rr[root][right] = rr[root][right] / total
+                if rr[root][right] % 1 > 0:
+                    print(root, '->', *right, rr[root][right])
+                else:
+                    print(root, '->', *right, "{:d}".format(int(rr[root][right])))
+
+        for root in rl.keys():
+            if root in rr:
+                total = rr[root]['root_count'] + rl[root]['root_count']
             else:
-                print(key, "{:d}".format(int(p)))
+                total = rl[root]['root_count']
+            for right in rl[root].keys():
+                if right == 'root_count':
+                    continue
+
+                rl[root][right] = rl[root][right] / total
+                if rl[root][right] % 1 > 0:
+                    print(root, right, rl[root][right])
+                else:
+                    print(root, right, "{:d}".format(int(rl[root][right])))
+            if root in rr:
+                del rr[root]['root_count']
+            del rl[root]['root_count']
 
         for word in w:
             print(word)
     # calculate probabilities and save data in files
     else:
         f = open(name + '.rules', "w")
-        for key in nrr:
-            root, rest = key.split(' ', 1)
-            p = nrr[key] / nn[root]
-            if p % 1 > 0:
-                f.write(key + ' ' + str(p) + '\n')
+        for root in rr.keys():
+            if root in rl:
+                total = rr[root]['root_count'] + rl[root]['root_count']
             else:
-                f.write(key + ' ' + "{:d}".format(int(p)) + '\n')
+                total = rr[root]['root_count']
+            for right in rr[root].keys():
+                if right == 'root_count':
+                    continue
+
+                rr[root][right] = rr[root][right] / total
+                if rr[root][right] % 1 > 0:
+                    f.write(root + ' -> ' + ' '.join(map(str, right)) + ' ' + str(rr[root][right]) + '\n')
+                else:
+                    f.write(
+                        root + ' -> ' + ' '.join(map(str, right)) + ' ' + "{:d}".format(int(rr[root][right])) + '\n')
         f.close()
 
         f = open(name + '.lexicon', "w")
-        for key in nrl:
-            root, rest = key.split(' ', 1)
-            p = nrl[key] / nn[root]
-            if p % 1 > 0:
-                f.write(key + ' ' + str(p) + '\n')
-            else: 
-                f.write(key + ' ' + "{:d}".format(int(p)) + '\n')
+        for root in rl.keys():
+            if root in rr:
+                total = rr[root]['root_count'] + rl[root]['root_count']
+            else:
+                total = rl[root]['root_count']
+            for right in rl[root].keys():
+                if right == 'root_count':
+                    continue
+
+                rl[root][right] = rl[root][right] / total
+                if rl[root][right] % 1 > 0:
+                    f.write(root + ' ' + str(right) + ' ' + str(rl[root][right]) + '\n')
+                else:
+                    f.write(root + ' ' + str(right) + ' ' + "{:d}".format(int(rl[root][right])) + '\n')
+            if root in rr:
+                del rr[root]['root_count']
+            del rl[root]['root_count']
         f.close()
 
         f = open(name + '.words', "w")
