8a4cebefae82099bebb8a7a2d4d81486201647c1
Christina Eggert <chreg605@student.liu.se>
2021-06-09 14:50:36 +0200

    reconstruct rules to increase performance

--- parse_phrases.py
+++ parse_phrases.py
@@ -8,39 +8,65 @@ LexicalRule = collections.namedtuple('LexicalRule', ['word', 'probability'])
 
 
 def read_rulesr(file):
-    rr = {}
+    rr = {}  # rr[rhs0][rhs1][lhs] = w
+    potential_start = set()
+    nts = set()
+
     for rule in open(file):  # rule = "A -> A1 .. An w[\n]"
         # get rid of \n if present
         if rule[len(rule) - 1] == '\n':
             rule = rule[0:len(rule) - 1]
         rule = rule.split(" ")  # get list of tokens
         lhs = rule.pop(0)  # first element is root
+        potential_start.add(lhs)
         w = rule.pop(len(rule) - 1)  # last element is the probability
         rhs = rule[1:len(rule)]  # cut '->' to get the right hand side of the rule
+        nts.update(rhs)
         # binarise rule
         while len(rhs) > 2:
             new_nt = rhs[len(rhs) - 2] + '_' + rhs[len(rhs) - 1]  # combine last two nonterminals to new one
-            rr[new_nt] = {(rhs.pop(len(rhs) - 2), rhs.pop(len(rhs) - 1)): 1.0}  # add new rule
+            rhs0 = rhs.pop(len(rhs) - 2)
+            rhs1 = rhs.pop(len(rhs) - 2)
+            if rhs0 not in rr:
+                rr[rhs0] = {}
+            if rhs1 not in rr[rhs0]:
+                rr[rhs0][rhs1] = {}
+            rr[rhs0][rhs1][new_nt] = 1.0  # add new rule
             rhs.append(new_nt)
-        if lhs in rr:
-            rr[lhs].update({tuple(rhs): float(w)})
-        else:
-            rr[lhs] = {tuple(rhs): float(w)}
+        if rhs[0] not in rr:
+            rr[rhs[0]] = {}
+        if len(rhs) == 1:
+            rhs.append('none')
+        if rhs[1] not in rr[rhs[0]]:
+            rr[rhs[0]][rhs[1]] = {}
+        rr[rhs[0]][rhs[1]][lhs] = float(w)
 
-    return rr
+    # find start symbol
+    start = ''
+    potential_start.difference_update(nts)
+    if len(potential_start) == 1:
+        start = potential_start.pop()
+    elif len(potential_start) == 0:
+        print('ERROR: no start symbol found.', file=sys.stderr)
+        exit(1)
+    else:
+        print('ERROR: ambiguous start symbol.', file=sys.stderr)
+        exit(1)
+
+    return rr, start
 
 
 def read_rulesl(file):
-    rl = {}
+    rl = {}  # rl[word][lhs] = w
     for rule in open(file):  # rule = "A v w[\n]"
         # get rid of \n if present
         if rule[len(rule) - 1] == '\n':
             rule = rule[0:len(rule) - 1]
         lhs, rhs, w = rule.split(" ")
-        if lhs in rl:
-            rl[lhs].update({rhs: float(w)})
+        if rhs in rl:
+            rl[rhs].update({lhs: float(w)})
         else:
-            rl[lhs] = {rhs: float(w)}
+            rl[rhs] = {lhs: float(w)}
 
     return rl
 
@@ -65,12 +91,12 @@ def get_branch(lhs, c, i, j):
     return False
 
 
-def construct_ptb_tree(c, start_rr, new_root):
-    if c[0][len(c)] == {} or start_rr not in c[0][len(c)]:
+def construct_ptb_tree(c, start, new_start):
+    if c[0][len(c)] == {} or start not in c[0][len(c)]:
         return False
 
-    penn_tree = get_branch(start_rr, c, 0, len(c))
-    penn_tree = penn_tree.replace(start_rr, new_root)
+    penn_tree = get_branch(start, c, 0, len(c))
+    penn_tree = penn_tree.replace(start, new_start)
     return penn_tree
 
 
@@ -84,40 +110,25 @@ def unary_closure(rr, c, i, j):
         if c[new_rhs].probability < q:
             c[new_rhs] = c[new_rhs]._replace(probability=q)
 
-            for new_lhs, rules in rr.items():
-                if (new_rhs,) in rules:
-                    # already existing derivations can not be overridden to avoid infinite loops
-                    if new_lhs not in c:
-                        queue.append((new_lhs, rr[new_lhs][(new_rhs,)] * q))
-                        # save used rule for back tracing
-                        c[new_lhs] = UnaryRule((new_rhs,), 0, (i, j))
+            if new_rhs in rr:
+                if 'none' in rr[new_rhs]:
+                    for new_lhs in rr[new_rhs]['none']:
+                        # already existing derivations can not be overridden to avoid infinite loops
+                        if new_lhs not in c:
+                            queue.append((new_lhs, rr[new_rhs]['none'][new_lhs] * q))
+                            # save used rule for back tracing
+                            c[new_lhs] = UnaryRule((new_rhs,), 0, (i, j))
+
     return c
 
 
 def parse_phrases_cyk(rules, lexicon, root):
-    rr = read_rulesr(rules)
-    # find start symbol: nt that only appears as lhs
-    nts = set(rr.keys())
-    for lhs, rules in rr.items():
-        for rhs in rules:
-            if rhs[0] in nts:
-                nts.remove(rhs[0])
-            if len(rhs) == 2 and rhs[1] in nts:
-                nts.remove(rhs[1])
-    # if there are several: error no unambiguous start symbol
-    if len(nts) == 1:
-        s = nts.pop()
-    elif len(nts) == 0:
-        print('ERROR: no start symbol found.', file=sys.stderr)
-        exit(1)
-    else:
-        print('ERROR: ambiguous start symbol.', file=sys.stderr)
-        exit(1)
+    rr, s = read_rulesr(rules)
     rl = read_rulesl(lexicon)
 
     # testsentence: Not this year .
     for phrase in sys.stdin:
-        # TODO: escape sequences?
+        # escape sequences are my friends?
         phrase_str = ''
         if phrase[len(phrase) - 1] == '\n':
             phrase_str = phrase[0:len(phrase) - 1]
@@ -132,10 +143,10 @@ def parse_phrases_cyk(rules, lexicon, root):
 
         #  use lexical rules to turn terminals into nonterminals
         for i in range(len(phrase)):
-            for lhs, rules in rl.items():
-                if phrase[i] in rules:
-                    c[i][i + 1][lhs] = LexicalRule(phrase[i], rl[lhs][phrase[i]])
-            c[i][i + 1] = unary_closure(rr, c[i][i + 1], i, i + 1)
+            if phrase[i] in rl:
+                for lhs in rl[phrase[i]]:
+                    c[i][i + 1][lhs] = LexicalRule(phrase[i], rl[phrase[i]][lhs])
+                c[i][i + 1] = unary_closure(rr, c[i][i + 1], i, i + 1)
 
         # lhs, w = max(c[0][1].items(), key=itemgetter(1))
         # print(lhs + ':' + str(w))
@@ -145,23 +156,19 @@ def parse_phrases_cyk(rules, lexicon, root):
             for i in range(0, len(phrase) - r + 1):
                 j = i + r
 
-                for lhs, rules in rr.items():
-                    for rhs in rules:
-                        for m in range(i + 1, j):
-                            if len(rhs) > 1:
-                                child1 = c[i][m].get(rhs[0], 'none')
-                                child2 = c[m][j].get(rhs[1], 'none')
-                                if child1 != 'none' and child2 != 'none':
-                                    prev_val = c[i][j].get(lhs, 'none')
-                                    new_val = rr[lhs][rhs] * child1.probability * child2.probability
-                                    if prev_val == 'none' or prev_val[1] < new_val:
-                                        c[i][j][lhs] = BinaryRule(rhs, new_val, (i, m), (m, j))
-                            else:
-                                continue
+                for m in range(i + 1, j):
+                    for rhs0 in c[i][m]:
+                        if rhs0 in rr:
+                            for rhs1 in c[m][j]:
+                                if rhs1 in rr[rhs0]:
+                                    for lhs in rr[rhs0][rhs1]:
+                                        prev_val = c[i][j].get(lhs, 'none')
+                                        new_val = rr[rhs0][rhs1][lhs] * c[i][m][rhs0].probability * c[m][j][rhs1].probability
+                                        if prev_val == 'none' or prev_val[1] < new_val:
+                                            c[i][j][lhs] = BinaryRule((rhs0, rhs1), new_val, (i, m), (m, j))
 
                 c[i][j] = unary_closure(rr, c[i][j], i, j)
 
-        # TODO: variable root
         tree = construct_ptb_tree(c, s, root)
         if not tree:
             print('(NOPARSE ' + phrase_str + ')')
